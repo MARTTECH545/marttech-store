0dffa09c5de49cf28e725e077fdf12d667f04593
MARTTECH545 <129175726+MARTTECH545@users.noreply.github.com>
2023-03-28 18:56:17 +0200

    Add files via upload

--- PropertyFetch.php
+++ PropertyFetch.php
@@ -2,39 +2,34 @@
 
 namespace PhpParser\Node\Expr;
 
-use PhpParser\Node\Arg;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Identifier;
 
-class MethodCall extends Expr
+class PropertyFetch extends Expr
 {
     /** @var Expr Variable holding object */
     public $var;
-    /** @var Identifier|Expr Method name */
+    /** @var Identifier|Expr Property name */
     public $name;
-    /** @var Arg[] Arguments */
-    public $args;
 
     /**
      * Constructs a function call node.
      *
      * @param Expr                   $var        Variable holding object
-     * @param string|Identifier|Expr $name       Method name
-     * @param Arg[]                  $args       Arguments
+     * @param string|Identifier|Expr $name       Property name
      * @param array                  $attributes Additional attributes
      */
-    public function __construct(Expr $var, $name, array $args = [], array $attributes = []) {
+    public function __construct(Expr $var, $name, array $attributes = []) {
         $this->attributes = $attributes;
         $this->var = $var;
         $this->name = \is_string($name) ? new Identifier($name) : $name;
-        $this->args = $args;
     }
 
     public function getSubNodeNames() : array {
-        return ['var', 'name', 'args'];
+        return ['var', 'name'];
     }
     
     public function getType() : string {
-        return 'Expr_MethodCall';
+        return 'Expr_PropertyFetch';
     }
 }
--- StaticCall.php
+++ StaticCall.php
@@ -2,39 +2,39 @@
 
 namespace PhpParser\Node\Expr;
 
-use PhpParser\Node\Arg;
+use PhpParser\Node;
 use PhpParser\Node\Expr;
 use PhpParser\Node\Identifier;
 
-class MethodCall extends Expr
+class StaticCall extends Expr
 {
-    /** @var Expr Variable holding object */
-    public $var;
+    /** @var Node\Name|Expr Class name */
+    public $class;
     /** @var Identifier|Expr Method name */
     public $name;
-    /** @var Arg[] Arguments */
+    /** @var Node\Arg[] Arguments */
     public $args;
 
     /**
-     * Constructs a function call node.
+     * Constructs a static method call node.
      *
-     * @param Expr                   $var        Variable holding object
+     * @param Node\Name|Expr         $class      Class name
      * @param string|Identifier|Expr $name       Method name
-     * @param Arg[]                  $args       Arguments
+     * @param Node\Arg[]             $args       Arguments
      * @param array                  $attributes Additional attributes
      */
-    public function __construct(Expr $var, $name, array $args = [], array $attributes = []) {
+    public function __construct($class, $name, array $args = [], array $attributes = []) {
         $this->attributes = $attributes;
-        $this->var = $var;
+        $this->class = $class;
         $this->name = \is_string($name) ? new Identifier($name) : $name;
         $this->args = $args;
     }
 
     public function getSubNodeNames() : array {
-        return ['var', 'name', 'args'];
+        return ['class', 'name', 'args'];
     }
     
     public function getType() : string {
-        return 'Expr_MethodCall';
+        return 'Expr_StaticCall';
     }
 }
